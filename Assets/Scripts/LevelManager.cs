e387677f498e47e43bd64b3389c306c2165a4da6
Brant Wagner <brawagne@uat.edu>
2018-08-14 14:10:29 -0700

    Add files via upload

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public GameObject currentCheckpoint;
+
+    private ControllerPlayer player;
+
+	// Use this for initialization
+	void Start () {
+        player = FindObjectOfType<ControllerPlayer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void RespawnPlayer()
+    {
+        Debug.Log("Player Respwan");
+        player.transform.position = currentCheckpoint.transform.position; 
+    }
+}
